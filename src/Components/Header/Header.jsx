ca87baebf51d1854f945fdcaa145fa4519acfcb5
ABDULAZIZJEMALL <abdulazizjemal03@gmail.com>
2024-06-19 16:08:09 +0300

    first attempt

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,96 @@
+import React, { useContext } from "react";
+import { BsSearch } from "react-icons/bs";
+import { SlLocationPin } from "react-icons/sl";
+import { BiCart } from "react-icons/bi";
+import { Link } from "react-router-dom";
+
+import classes from "./Header.module.css";
+import LowerHeader from "./LowerHeader";
+import { DataContext } from "../DataProvider/DataProvider";
+import { auth } from "../../Utility/firebase";
+function Header() {
+  const [{ user, basket }, dispatch] = useContext(DataContext);
+  const totalItem = basket?.reduce((amount, item) => {
+    return item.amount + amount;
+  }, 0);
+
+  return (
+    <section className={classes.fixed}>
+      <section className={classes.header_container}>
+        <div className={classes.logo_container}>
+          {/* logo */}
+          <Link to="/">
+            <img
+              src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+              alt="Amazon Logo"
+            />
+          </Link>
+          <div className={classes.delivery}>
+            {/* Delivery */}
+            <span>
+              {/* icon */}
+              <SlLocationPin />
+            </span>
+            <div>
+              <p>Delivered to</p>
+              <span>Ethiopia</span>
+            </div>
+          </div>
+        </div>
+        <div className={classes.search}>
+          {/* search */}
+          <select name="" id="">
+            <option value="">All</option>
+          </select>
+          <input type="text" name="" id="" placeholder="Search Product" />
+          {/* icon */}
+          <BsSearch size={38} />
+        </div>
+        {/* Right Side link */}
+        <div className={classes.order_container}>
+          <div>
+            <Link to="" className={classes.language}>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/800px-Flag_of_the_United_States.svg.png?20150326181342"
+                alt="USA Flag"
+              />
+              <section>
+                <option value="">EN</option>
+              </section>
+            </Link>
+          </div>
+          {/* three Components */}
+          <Link to={!user && "/auth"}>
+            <div>
+              {user ? (
+                <>
+                  <p>Hello {user?.email?.split("@")[0]}</p>
+                  <span onClick={() => auth.signOut()}>Log Out</span>
+                </>
+              ) : (
+                <>
+                  <p>Hello Sign In</p>
+                  <span>Account & Lists</span>
+                </>
+              )}
+            </div>
+          </Link>
+          {/* orders */}
+          <Link to="/orders">
+            <p>returns</p>
+            <span>& Orders</span>
+          </Link>
+          {/* cart */}
+          <Link to="/cart" className={classes.cart}>
+            {/* icon */}
+            <BiCart size={35} />
+            <span>{totalItem}</span>
+          </Link>
+        </div>
+      </section>
+      <LowerHeader />
+    </section>
+  );
+}
+
+export default Header;
